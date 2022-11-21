b98e3f556e4d2a41eb27d457a6fdc20423c3cf33
Afonso Machado <afonsosmachado@gmail.com>
2022-11-21 16:44:11 -0300

    card animation

--- components/FormCreditCard.vue
+++ components/FormCreditCard.vue
@@ -1,12 +1,19 @@
 <template>
   <form class="third-form">
-    <SwivelCard />
+    <SwivelCard
+      :cvv="card.cvv"
+      :expiration="card.expiration"
+      :holder="card.holder"
+      :number="card.number"
+      :card-position="cardPosition"
+    />
     <div class="form-input">
       <label for="credit-card-number"
         >Número do cartão <span class="required">*</span></label
       >
       <input
         id="credit-card-number"
+        v-model="card.number"
         v-mask="'#### #### #### ####'"
         class="rounded"
         type="text"
@@ -20,6 +27,7 @@
       >
       <input
         id="credit-card-holder"
+        v-model="card.holder"
         class="rounded"
         type="text"
         placeholder="Nome (igual ao cartão)"
@@ -43,20 +51,24 @@
         <label for="street"> Validade <span class="required">*</span></label>
         <input
           id="street"
-          v-mask="'##/####'"
+          v-model="card.expiration"
+          v-mask="'##/##'"
           class="rounded mr-2"
           type="text"
-          placeholder="MM/AAAA"
+          placeholder="MM/AA"
         />
       </div>
       <div class="form-input">
         <label for="number"> CVV <span class="required">*</span></label>
         <input
           id="number"
+          v-model="card.cvv"
           v-mask="'###'"
           class="rounded"
           type="text"
           placeholder="CVV"
+          @focus="turnCardBack"
+          @blur="turnCardFront"
         />
       </div>
     </div>
@@ -67,6 +79,26 @@
 import SwivelCard from './SwivelCard.vue'
 export default {
   components: { SwivelCard },
+  data() {
+    return {
+      card: {
+        number: '',
+        holder: '',
+        expiration: '',
+        cvv: '',
+      },
+      cardPosition: 'front',
+    }
+  },
+
+  methods: {
+    turnCardBack() {
+      this.cardPosition = 'back'
+    },
+    turnCardFront() {
+      this.cardPosition = 'front'
+    },
+  },
 }
 </script>
 
--- components/SwivelCard.vue
+++ components/SwivelCard.vue
@@ -1,10 +1,41 @@
 <template>
   <main class="swivel-card-wrapper">
-    <div class="swivel-card">
+    <div class="swivel-card" :class="cardPosition">
       <div class="swivel-card-content">
-        <div class="swivel-card-content-back"></div>
-        <div class="swivel-card-content-front">
-          <div class="card-top d-flex justify-content-between w-100">
+        <div
+          class="swivel-card-content-back w-100 d-flex justify-content-between"
+        >
+          <div class="card-top w-100">
+            <div class="card-band"></div>
+          </div>
+          <div class="card-mid d-flex w-100 justify-content-end">
+            <div class="cvv">
+              <span class="text-white font-weight-bold">CVV</span>
+              <div
+                class="
+                  cvv-box
+                  d-flex
+                  flex-column
+                  align-content-center
+                  justify-content-center
+                "
+              >
+                <span class="font-weight-bold">
+                  {{ cvv }}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card-bot w-100 d-flex justify-content-end">
+            <img
+              alt="Logo mastercadr"
+              src="../assets/images/ic-mastercard.png"
+              class="card-logo"
+            />
+          </div>
+        </div>
+        <div class="swivel-card-content-front d-flex justify-content-between">
+          <div class="card-top d-flex w-100 justify-content-between">
             <img
               alt="Chip cartão"
               src="../assets/images/ic-chip.png"
@@ -16,17 +47,17 @@
               class="card-logo"
             />
           </div>
-          <div class="card-mid">
-            <span class="card-number">#### #### #### ####</span>
+          <div class="card-mid d-flex w-100">
+            <span class="card-number text-left">{{ cardNumber }}</span>
           </div>
           <div class="card-bot d-flex justify-content-between w-100">
             <div class="card-holder d-flex flex-column text-left">
-              <span>Titular do cartão</span>
-              <span>afonso s machado</span>
+              <span class="font-weight-lighter">Titular do cartão</span>
+              <span>{{ cardHolder }}</span>
             </div>
-            <div class="validity d-flex flex-column text-right">
-              <span>Validade</span>
-              <span>MM/AA</span>
+            <div class="expiration d-flex flex-column text-right">
+              <span class="font-weight-lighter">Validade</span>
+              <span>{{ cardExpiration }}</span>
             </div>
           </div>
         </div>
@@ -36,30 +67,61 @@
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    number: {
+      type: String,
+      default: '#### #### #### ####',
+    },
+    holder: {
+      type: String,
+      default: 'nome impresso',
+    },
+    expiration: {
+      type: String,
+      default: 'MM/AA',
+    },
+    cvv: {
+      type: String,
+      default: 'CVV',
+    },
+    cardPosition: {
+      type: String,
+      default: 'front',
+    },
+  },
+
+  computed: {
+    cardNumber() {
+      return this.number === '' ? '#### #### #### ####' : this.number
+    },
+    cardExpiration() {
+      return this.expiration === '' ? 'MM/AA' : this.expiration
+    },
+    cardHolder() {
+      return this.holder === '' ? 'nome impresso' : this.holder
+    },
+  },
+}
 </script>
 
 <style>
-.swivel-card-wrapper {
-  background-size: 100% center;
-  background-position: center;
-  background-repeat: no-repeat;
-  display: grid;
-  place-items: center;
-}
-
 .swivel-card {
   background-color: transparent;
   width: 100%;
   height: 240px;
   perspective: 1000px;
-  cursor: pointer;
   margin-bottom: 15px;
 }
 
 .swivel-card-content {
   border-radius: 20px;
-  background-color: aquamarine;
+  background: rgb(26, 27, 30);
+  background: radial-gradient(
+    circle,
+    rgba(26, 27, 30, 1) 0%,
+    rgba(92, 114, 233, 1) 79%
+  );
   cursor: pointer;
   position: relative;
   width: 100%;
@@ -69,7 +131,20 @@ export default {}
   transform-style: preserve-3d;
 }
 
-.swivel-card:hover .swivel-card-content {
+.cvv-box {
+  height: 40px;
+  width: 40px;
+  background-color: #fff;
+  border-radius: 5px;
+}
+
+.card-band {
+  background-color: #01021a;
+  height: 50px;
+  width: 100%;
+}
+
+.swivel-card.back .swivel-card-content {
   transform: rotateY(180deg);
 }
 
@@ -78,11 +153,8 @@ export default {}
   position: absolute;
   width: 100%;
   height: 100%;
-  -webkit-backface-visibility: hidden;
-  /* Safari */
   backface-visibility: hidden;
   border-radius: 15px;
-  cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -95,7 +167,6 @@ export default {}
   border-radius: 20px;
   box-shadow: 0 4px 30px rgba(255, 255, 255, 0.15), inset 0 0 32px -11px white;
   backdrop-filter: blur(5px);
-  -webkit-backdrop-filter: blur(5px);
   border: 1px solid rgba(255, 255, 255, 0.2);
 }
 
@@ -114,7 +185,7 @@ export default {}
 }
 
 .card-holder,
-.validity {
+.expiration {
   font-size: 18px;
 }
 
@@ -122,7 +193,7 @@ export default {}
   text-transform: uppercase;
 }
 
-.swivel-card-content-front .card-logo {
+.card-logo {
   width: 50px;
   height: auto;
   filter: drop-shadow(2px 2px 5px rgba(0, 0, 0, 0.4));
@@ -135,7 +206,15 @@ export default {}
 .swivel-card-content-front {
   color: white;
   padding: 16px 20px;
-  display: flex;
-  justify-content: space-between;
+}
+
+.swivel-card-content-back {
+  border: none;
+  padding: 16px 0;
+}
+
+.swivel-card-content-back .card-mid,
+.swivel-card-content-back .card-bot {
+  padding-right: 20px;
 }
 </style>
